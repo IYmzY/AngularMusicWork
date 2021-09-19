5262826e1646350be1bcb298131d7b183cbdc63f
YmzY <redanabil2199@gmail.com>
2021-09-19 19:14:50 +0200

    product

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { ConfigService } from '../config.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {   
 }
+}
+ 
